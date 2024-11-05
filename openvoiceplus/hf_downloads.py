1134d6d3159089acd7709d755beed2c66e017d41
kadirnar <kadir.nar@hotmail.com>
2024-11-05 21:57:22 +0000

    Add melotts support to OpenVoice library

--- openvoiceplus/hf_downloads.py
+++ openvoiceplus/hf_downloads.py
@@ -1,8 +1,21 @@
 from huggingface_hub import snapshot_download
 
-snapshot_download(
-    repo_id="myshell-ai/OpenVoice",
-    repo_type="model",
-    ignore_patterns=["*.md", "*..gitattributes"],
-    local_dir="checkpoints",
-)
+
+def download_openvoice_model(model_version="v1"):
+    """
+    Download OpenVoice model based on version.
+
+    Args:
+        model_version (str): Version of the model ("v1" or "v2")
+    """
+    if model_version.lower() == "v1":
+        repo_id = "myshell-ai/OpenVoice"
+        local_dir = "./"
+    elif model_version.lower() == "v2":
+        repo_id = "myshell-ai/OpenVoiceV2"
+        local_dir = "checkpoints_v2"
+    else:
+        raise ValueError("model_version must be either 'v1' or 'v2'")
+
+    snapshot_download(
+        repo_id=repo_id, repo_type="model", ignore_patterns=["*.md", "*..gitattributes"], local_dir=local_dir)
